1c94add34a3c4dd2e4f6439af016b4fb09f66b2b
villelatypebot <152432766+villelatypebot@users.noreply.github.com>
2025-05-06 20:06:03 -0300

    Add files via upload

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,22 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
+import path from 'path'
 
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+  build: {
+    outDir: 'dist',
+    rollupOptions: {
+      input: {
+        main: path.resolve(process.cwd(), 'index.html'),
+      },
+    },
+  },
+  server: {
+    port: 5173
+  },
+  preview: {
+    port: 10000
+  }
 }) 
